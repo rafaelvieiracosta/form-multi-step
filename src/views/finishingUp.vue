3d096da841a90e15228eaab27b3022748aa0a3e8
rafaelvieiracosta <rvc.vieira@outlook.com>
2023-03-24 16:39:37 -0300

    feat: init page `finishingUp`

--- src/views/finishingUp.vue
+++ src/views/finishingUp.vue
@@ -1,6 +1,50 @@
 <template>
-  <section>
-    <h1>Finishing up</h1>
+  <section id="pickAddOns" class="section">
+    <h1 class="section-title">Finalizando</h1>
+    <p class="section-description">
+      Verifique se tudo está OK antes de confirmar.
+    </p>
+
+    <div class="section-resume">
+      <div class="section-resume-plan">
+        <div class="section-resume-plan-name">
+          <h2>{{ planData.name }} ({{ planData.periodSelected }})</h2>
+          <router-link to="/plan">Mudar</router-link>
+        </div>
+
+        <p
+          v-if="planData.periodSelected === 'yearly'"
+          class="section-resume-plan-price"
+        >
+          {{ planData.price.yearly | formattedPrice }}/ano
+        </p>
+        <p v-else class="section-resume-plan-price">
+          {{ planData.price.monthly | formattedPrice }}/mês
+        </p>
+      </div>
+
+      <div class="section-resume-addons">
+        <div
+          v-for="item in this.$store.state.pickAddOns.addOns"
+          :key="item.id"
+          class="section-resume-addons-item"
+        >
+          <p class="section-resume-addons-item-name">
+            {{ item.name }}
+          </p>
+
+          <p
+            v-if="planData.periodSelected === 'yearly'"
+            class="section-resume-addons-item-price"
+          >
+            +{{ item.price.yearly | formattedPrice }}/ano
+          </p>
+          <p v-else class="section-resume-addons-item-price">
+            +{{ item.price.monthly | formattedPrice }}/mês
+          </p>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -10,7 +54,19 @@ export default {
   data() {
     return {};
   },
+  computed: {
+    planData() {
+      return this.$store.state.selectYourPlan.plan;
+    },
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.section-resume {
+  margin-top: 32px;
+  background-color: var(--c1);
+  border-radius: 7px;
+  padding: 20px 24px;
+}
+</style>
